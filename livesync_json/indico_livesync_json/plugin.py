151e77d37c7a86f5b45178352307f1020a1f3dbb
penelope <penelope@fnal.gov>
2019-10-09 12:44:21 -0500

    Update plugin.py

--- livesync_json/indico_livesync_json/plugin.py
+++ livesync_json/indico_livesync_json/plugin.py
@@ -8,7 +8,7 @@
 from __future__ import unicode_literals
 
 from indico_livesync import LiveSyncPluginBase
-from indico_livesync_json.backend import LivesyncJsonBackend, LivesyncJsonAgentForm
+from indico_livesync_json.backend import LivesyncJsonBackend, LivesyncJsonForm
 from indico_livesync_json.blueprint import blueprint
 
 
@@ -19,7 +19,7 @@ class LivesyncJsonPlugin(LiveSyncPluginBase):
     """
 
     configurable = True
-    settings_form = LivesyncJsonAgentForm
+    settings_form = LivesyncJsonForm
     default_settings = {'search_app_url': '',
                         'search_app_token': '', 
                         'es_events': 'events_v1.1.0.json',
