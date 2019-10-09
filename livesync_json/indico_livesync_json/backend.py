ab6093e464bb07a26768e4697a32b9d62054a3a0
penelope <penelope@fnal.gov>
2019-10-09 15:08:00 -0500

    Update backend.py

--- livesync_json/indico_livesync_json/backend.py
+++ livesync_json/indico_livesync_json/backend.py
@@ -21,37 +21,19 @@ from indico.modules.events.contributions.models.subcontributions import SubContr
 from indico.modules.attachments import Attachment
 from indico.modules.events.notes.models.notes import EventNote
 
-from indico_livesync_json.models.search_id_map import EntryType, Livesync_json_search_id_map_entry
+from indico_livesync_json.plugin import livesyncjson_settingsform
+from indico_livesync_json.models.search_id_map import EntryType, livesyncjson_searchapp_id_map
 from indico_livesync_json.schemas import EventSchema, ContributionSchema, SubContributionSchema, AttachmentSchema, EventNoteSchema
-from indico_livesync import AgentForm, LiveSyncBackendBase, SimpleChange
+from indico_livesync import LiveSyncBackendBase, SimpleChange
 from indico_livesync import Uploader
 from indico_livesync_json import _
 
 
-class LivesyncJsonForm(IndicoForm):
-    search_app_url = URLField(_('Search app URL'), [DataRequired(), URL(require_tld=False)],
-                          description=_("URL <url:port> of search app import endpoint"))
-    search_app_token = StringField(_('Search app TOKEN'), [DataRequired()],
-                          description=_("TOKEN  for accessing the Search app import endpoint"))
-    es_events = StringField(_('Elasticsearch Events JSON Schema'), [DataRequired()],
-                          description=_("<events_vn.n.n.json>: the JSON Schema for the events Elasticsearch index"))
-    es_contributions = StringField(_('Elasticsearch Contributions JSON Schema'), [DataRequired()],
-                          description=_("<contributions_vn.n.n.json>: the JSON Schema for the contributions Elasticsearch index"))
-    es_subcontributions = StringField(_('Elasticsearch SubContributions JSON Schema'), [DataRequired()],
-                          description=_("<subcontributions_vn.n.n.json>: the JSON Schema for the subcontributions Elasticsearch index"))
-    es_attachments = StringField(_('Elasticsearch Attachments JSON Schema'), [DataRequired()],
-                          description=_("<attachments_vn.n.n.json>: the JSON Schema for the attachments Elasticsearch index"))
-    es_notes = StringField(_('Elasticsearch Notes JSON Schema'), [DataRequired()],
-                          description=_("<notes_vn.n.n.json>: the JSON Schema for the notes Elasticsearch index"))
-    tika_server = URLField(_('tika server URL'), [DataRequired(), URL(require_tld=False)],
-                          description=_("URL <url:port> of tika server to parse file content. If not supplied a local tika server will be instantiated."))
-
-
-class json_uploaderError(Exception):
+class livesyncjson_uploaderError(Exception):
     pass
 
 
-class json_uploader(Uploader):
+class livesyncjson_uploader(Uploader):
 
     def __init__(self, *args, **kwargs): 
         _search_app = self.backend.agent.settings.get('search_app_url').rstrip('/')
@@ -77,9 +59,9 @@ class json_uploader(Uploader):
 #            self.tika_server = self.backend.agent.settings.get('tika_server')
 #        else:
 #            import tika
-#            from indico_livesync_json.plugin import LivesyncJsonPlugin
+#            from indico_livesync_json.plugin import LiveSyncJsonPlugin
 #            tika.initVM()
-#            LivesyncJsonPlugin.settings.set('tika_server') = 'http://localhost:9998'
+#            LiveSyncJsonPlugin.settings.set('tika_server') = 'http://localhost:9998'
 #            self.tika_server = 'http://localhost:9998'
 
     def upload_records(self, records, from_queue):
@@ -121,7 +103,7 @@ class json_uploader(Uploader):
         if change_type == SimpleChange.created:
             response = requests.post(self.search_url, headers=self.headers, json=jsondata)
         else:
-            search_id = Livesync_json_search_id_map_entry.get_search_id(obj_id, entry_type)
+            search_id = livesyncjson_searchapp_id_map.get_search_id(obj_id, entry_type)
             if search_id:
                 if change_type == SimpleChange.updated:
                     response = requests.put('{}/{}'.format(self.search_url, search_id), headers=self.headers, 
@@ -135,19 +117,18 @@ class json_uploader(Uploader):
                 pass
 
         if response.status_code != 200:
-            raise json_uploaderError('{} - {}'.format(response.status_code, response.text))
+            raise livesyncjson_uploaderError('{} - {}'.format(response.status_code, response.text))
         elif change_type == SimpleChange.created:
             if response.id:
-                Livesync_json_search_id_map_entry.create(response.id, obj_id, entry_type)
+                livesyncjson_searchapp_id_map.create(response.id, obj_id, entry_type)
             else:
-                raise json_uploaderError('Cannot create the search id mapping: {} - {}'.format(response.status_code, response.text))
+                raise livesyncjson_uploaderError('Cannot create the search id mapping: {} - {}'.format(response.status_code, response.text))
 
 
-class LivesyncJsonBackend(LiveSyncBackendBase):
+class livesyncjson_backend(LiveSyncBackendBase):
     """JSON-search
 
     This backend uploads data to JSON-search.
     """
 
-    uploader = json_uploader
-    form = LivesyncJsonForm
+    uploader = livesyncjson_uploader
